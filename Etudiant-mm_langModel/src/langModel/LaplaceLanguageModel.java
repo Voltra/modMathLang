c1f1ff21171f6b1446bd17f43f5d588b7bc4704c
Voltra <ludwig.guerin.98@gmx.fr>
2017-12-18 12:12:01 +0100

    Completed LaplaceLanguageModel

--- Etudiant-mm_langModel/src/langModel/LaplaceLanguageModel.java
+++ Etudiant-mm_langModel/src/langModel/LaplaceLanguageModel.java
@@ -1,6 +1,8 @@
 package langModel;
 
 
+import java.util.Objects;
+
 /**
  * Class LaplaceLanguageModel: class inheriting the class NaiveLanguageModel by creating 
  * a n-gram language model using a Laplace smoothing.
@@ -12,7 +14,18 @@ public class LaplaceLanguageModel extends NaiveLanguageModel {
 
 	@Override
 	public Double getNgramProb(String ngram) {
-		//TODO
-		return 0.0;
+		NoNullParams.assertNoneNull(ngram);
+
+		if(Objects.isNull(this.ngramCounts) || Objects.isNull(this.vocabulary))
+		    return Double.NaN;
+
+        int order = this.getLMOrder();
+        double words_amount = this.vocabulary.getSize();
+		String history = NgramUtils.getHistory(ngram, order);
+
+		double ngram_occ = this.ngramCounts.getCounts(ngram);
+        double hist_occ = this.ngramCounts.getCounts(history);
+
+        return (1 + ngram_occ) / (hist_occ + words_amount - 1);
 	}
 }
