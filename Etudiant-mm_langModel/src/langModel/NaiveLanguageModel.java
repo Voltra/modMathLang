2dc78c5ca20bd82651479b023179d90b1917fde1
CobraDarkPoney <CobraDarkPoney@gmail.com>
2017-12-21 08:44:27 +0100

    NaiveModelLanguage.java

--- Etudiant-mm_langModel/src/langModel/NaiveLanguageModel.java
+++ Etudiant-mm_langModel/src/langModel/NaiveLanguageModel.java
@@ -24,31 +24,36 @@ public class NaiveLanguageModel implements LanguageModelInterface {
 	 * Constructor.
 	 */
 	public NaiveLanguageModel(){
-		//TODO
+		this.ngramCounts = new NgramCounts();
+		this.vocabulary = new Vocabulary();
 	}
 	
 
 	@Override
 	public int getLMOrder() {
-		// TODO Auto-generated method stub
-		return 0;
+		return ngramCounts.getMaximalOrder();
 	}
 
 	@Override
 	public void setNgramCounts(NgramCountsInterface ngramCounts, VocabularyInterface vocab) {
-		// TODO Auto-generated method stub
+		this.ngramCounts = ngramCounts;
+		this.vocabulary = vocab;
 	}
 
 	@Override
 	public Double getNgramProb(String ngram) {
-		// TODO Auto-generated method stub
-		return null;
+        if (ngram.equals(Vocabulary.SOS_TAG))
+            return 1.0;
+        else if (ngramCounts.getMaximalOrder() == 1 && !ngram.equals(Vocabulary.SOS_TAG))
+            return new Double(ngramCounts.getCounts(ngram)/ngramCounts.getTotalWordNumber());
+        else if (ngramCounts.getMaximalOrder() > 2 && !ngram.equals(Vocabulary.EOS_TAG) && ngramCounts.getCounts(ngram) != 0)
+            return new Double(ngramCounts.getCounts(ngram)/ngramCounts.getCounts(NgramUtils.getHistory(ngram, ngramCounts.getMaximalOrder())));
+        else return 0.0;
 	}
 
 	@Override
 	public Double getSentenceProb(String sentence) {
-		// TODO Auto-generated method stub
-		return null;
+		return NgramUtils.decomposeIntoNgrams(sentence, ngramCounts.getMaximalOrder()).stream().map(this::getNgramProb).reduce(1.0, (probaTotal, probaNgram) -> probaNgram*probaTotal);
 	}
 
 }
