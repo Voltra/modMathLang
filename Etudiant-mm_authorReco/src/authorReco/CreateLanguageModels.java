638b9c1bb9bc34c96e78bc17e2aa0305c441ae6e
CobraDarkPoney <CobraDarkPoney@gmail.com>
2018-01-24 17:34:16 +0100

    Fix conflcits

--- Etudiant-mm_authorReco/src/authorReco/CreateLanguageModels.java
+++ Etudiant-mm_authorReco/src/authorReco/CreateLanguageModels.java
@@ -3,6 +3,8 @@ package authorReco;
 
 import langModel.*;
 
+import java.io.File;
+import java.util.List;
 
 
 /**
@@ -19,7 +21,19 @@ public class CreateLanguageModels {
 	 * @param args arguments of the main method.
 	 */
 	public static void main(String[] args) {
-		//create the language models from the training corpora
+        File folder = new File("data/author_corpus/train");
+        File[] listOfFiles = folder.listFiles();
+
+        for (File file : listOfFiles) {
+            if (file.isFile()) {
+                System.out.println("Processing file " + file.getName());
+                Vocabulary vocab = new Vocabulary();
+                vocab.readVocabularyFile(file.getPath());
+                NgramCounts ngramCounts = new NgramCounts();
+                ngramCounts.scanTextFile(file.getPath(), vocab, 3);
+                String outputPath = ""
+            }
+        }
 	}
 
 }
